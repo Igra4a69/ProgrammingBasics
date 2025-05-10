98431583d1a5ceb2bc69c29f9cade1e3b3a32503
Pavsheto <pavsheto@gmail.com>
2025-05-10 13:24:14 +0300

    add Projects Creation

---
+++ 01HelloSoftUni/ProjectsCreation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectsCreation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string architectName = Console.ReadLine();
+        int projectCount = int.Parse(Console.ReadLine());
+        
+        int neededHours = projectCount * 3;
+        
+        Console.WriteLine($"The architect {architectName} will need {neededHours} hours to complete {projectCount} project/s.");
+    }
+}

182f69b520d7b4407700dc0b13e86ccceffa079e
Pavsheto <pavsheto@gmail.com>
2025-05-10 17:42:21 +0300

    add Deposit Calculator

---
+++ FirstStepsInCoding-Exercise/DepositCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DepositCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double depositedAmount = double.Parse(Console.ReadLine());
+        int depositPeriodMonths = int.Parse(Console.ReadLine());
+        double annualInterestRate = double.Parse(Console.ReadLine());
+        
+        double sum = depositedAmount + depositPeriodMonths * ((depositedAmount * annualInterestRate / 100) / 12);
+
+        Console.WriteLine($"{sum:F2}");
+    }
+}

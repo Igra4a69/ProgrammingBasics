3d4fcf9994a0b49b98c6b6d3b836eda429448cdc
Pavsheto <pavsheto@gmail.com>
2025-05-10 12:55:12 +0300

    add Inches to Centimeters

---
+++ 01HelloSoftUni/InchestoCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace InchestoCentimeters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Въведете инчове: ");
+        double inches = double.Parse(Console.ReadLine());
+        
+        double centimeters = inches * 2.54;
+        
+        Console.WriteLine($"{centimeters:F2} см");
+    }
+}

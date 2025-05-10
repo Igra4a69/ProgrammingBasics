f89bc16f7bb10b52cd9bd29bbd1890aaad401330
Pavsheto <pavsheto@gmail.com>
2025-05-10 13:29:15 +0300

    add Pet Shop

---
+++ 01HelloSoftUni/PetShop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PetShop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dogPacks = int.Parse(Console.ReadLine());
+        int catPacks = int.Parse(Console.ReadLine());
+        
+        double dogFoodPrice = 2.50;
+        double catFoodPrice = 4.00;
+        
+        double total = (dogPacks * dogFoodPrice) + (catPacks * catFoodPrice);
+        
+        Console.WriteLine($"{total} lv.");
+    }
+}

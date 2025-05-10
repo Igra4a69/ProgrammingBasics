9c96004f50f2b8c3af335d55a66fb5256989b56e
Pavsheto <pavsheto@gmail.com>
2025-05-10 13:32:14 +0300

    add Yard Greening

---
+++ 01HelloSoftUni/YardGreening/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace YardGreening;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double areaInSquareMeters = double.Parse(Console.ReadLine());
+        
+        double pricePerSquareMeter = 7.61;
+        
+        double initialPrice = areaInSquareMeters * pricePerSquareMeter;
+        
+        double discountPercentage = 0.18; // 18%
+        
+        double discountAmount = initialPrice * discountPercentage;
+        
+        double finalPrice = initialPrice - discountAmount;
+        
+        Console.WriteLine($"The final price is: {finalPrice:F2} lv.");
+        Console.WriteLine($"The discount is: {discountAmount:F2} lv.");
+    }
+}

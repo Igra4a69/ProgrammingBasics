2aace2110d44781d2b681ccc83560a7cd41d5d6e
Pavsheto <pavsheto@gmail.com>
2025-05-11 17:45:00 +0300

    add Invalid Number

---
+++ ConditionalStatementsAdvanced-Lab/InvalidNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace InvalidNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if ((number >= 100 && number <= 200) || number == 0)
+        {
+            // валидно е, не печатаме нищо
+        }
+        else
+        {
+            Console.WriteLine("invalid");
+        }
+    }
+}

5f95b66d330fda0e246a6df3158bfcbb616d7e49
Pavsheto <pavsheto@gmail.com>
2025-05-11 11:57:34 +0300

    add WorldSwimmingRecords

---
+++ ConditionalStatements-Exercise/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecord = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+            
+            double swimmingTime = distance * timePerMeter;
+            
+            double resistanceDelay = Math.Floor(distance / 15) * 12.5;
+            
+            double totalTime = swimmingTime + resistanceDelay;
+            
+            if (totalTime < worldRecord)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+            }
+            else
+            {
+                double timeDifference = totalTime - worldRecord;
+                Console.WriteLine($"No, he failed! He was {timeDifference:F2} seconds slower.");
+            }
+        }
+    }
+}

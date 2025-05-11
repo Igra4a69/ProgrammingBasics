213c258ebad0bc672f1027fe5750386d899a1797
Pavsheto <pavsheto@gmail.com>
2025-05-11 11:45:38 +0300

    add BonusScore

---
+++ ConditionalStatements-Exercise/BonusScore/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BonusScore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int points = int.Parse(Console.ReadLine());
+        
+        double bonusPoints = 0;
+        
+        if (points <= 100)
+        {
+            bonusPoints = 5;
+        }
+        else if (points <= 1000)
+        {
+            bonusPoints = points * 0.20;
+        }
+        else
+        {
+            bonusPoints = points * 0.10;
+        }
+        
+        if (points % 2 == 0)
+        {
+            bonusPoints += 1;
+        }
+        if (points % 10 == 5)
+        {
+            bonusPoints += 2;
+        }
+        
+        Console.WriteLine(bonusPoints);
+        Console.WriteLine(points + bonusPoints);
+    }
+}

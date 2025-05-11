af5ed19e972c27fb90f5a0ee2df6229249fb53c4
Pavsheto <pavsheto@gmail.com>
2025-05-11 17:02:48 +0300

    add Animal Type

---
+++ ConditionalStatementsAdvanced-Lab/AnimalType/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AnimalType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string anymal = Console.ReadLine();
+
+        switch (anymal)
+        {
+            case "dog":
+                Console.WriteLine("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}

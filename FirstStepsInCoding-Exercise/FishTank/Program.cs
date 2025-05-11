0a4ff53b53748ec61fda183cc41a707fff29439d
Pavsheto <pavsheto@gmail.com>
2025-05-11 10:54:42 +0300

    add Fish Tank

---
+++ FirstStepsInCoding-Exercise/FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FishTank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        double percent = double.Parse(Console.ReadLine());
+        
+        double volumeCm = length * width * height;
+        
+        double volumeLiters = volumeCm / 1000;
+        
+        double neededLiters = volumeLiters * (1 - percent / 100);
+
+        Console.WriteLine($"{neededLiters:F3}");
+    }
+}

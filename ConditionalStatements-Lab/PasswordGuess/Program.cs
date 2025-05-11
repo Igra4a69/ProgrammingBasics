8a0fef11f8ae343372214a2334fd15a9ca61a49d
Pavsheto <pavsheto@gmail.com>
2025-05-11 11:20:34 +0300

    add Password Guess

---
+++ ConditionalStatements-Lab/PasswordGuess/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PasswordGuess;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputPassword = Console.ReadLine();
+
+        string correctPassword = "s3cr3t!P@ssw0rd";
+
+        if (inputPassword == correctPassword)
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}

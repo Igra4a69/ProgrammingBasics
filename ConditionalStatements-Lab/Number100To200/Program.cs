27c8c805ac3791b926c142cb3f47e86661f2bcff
Pavsheto <pavsheto@gmail.com>
2025-05-11 11:25:40 +0300

    add NumberRange;

---
+++ ConditionalStatements-Lab/Number100To200/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace NumberRange;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Вход: четем цяло число
+        int number = int.Parse(Console.ReadLine());
+
+        // Проверки
+        if (number < 100)
+        {
+            Console.WriteLine("Less than 100");
+        }
+        else if (number <= 200)
+        {
+            Console.WriteLine("Between 100 and 200");
+        }
+        else
+        {
+            Console.WriteLine("Greater than 200");
+        }
+    }
+}

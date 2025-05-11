6e5f4c1bdd4ac30fa2a1a80869bf595cf2aff0de
Pavsheto <pavsheto@gmail.com>
2025-05-11 10:46:09 +0300

    add Repainting

---
+++ FirstStepsInCoding-Exercise/Repainting/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Repainting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int nylonQuantity = int.Parse(Console.ReadLine());
+        int paintQuantity = int.Parse(Console.ReadLine());
+        int thinnerQuantity = int.Parse(Console.ReadLine());
+        int hoursWork = int.Parse(Console.ReadLine());
+        
+        double nylonPrice = (nylonQuantity + 2) * 1.50;
+        double paintPrice = (paintQuantity * 1.10) * 14.50;
+        double thinnerPrice = thinnerQuantity * 5.00;
+        double bagsPrice = 0.40;
+        
+        double totalMaterialsPrice = nylonPrice + paintPrice + thinnerPrice + bagsPrice;
+        
+        double workPrice = totalMaterialsPrice * 0.30 * hoursWork;
+        
+        double totalPrice = totalMaterialsPrice + workPrice;
+        
+        Console.WriteLine(totalPrice);
+    }
+}

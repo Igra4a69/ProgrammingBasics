74d83502bbbba8b3b4fe89a92f657b15c7ff9989
Pavsheto <pavsheto@gmail.com>
2025-05-11 11:31:52 +0300

    add Area of Figures

---
+++ ConditionalStatements-Lab/AreaofFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AreaofFigures;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string shape = Console.ReadLine();
+
+        double area = 0;
+        
+        if (shape == "square")
+        {
+            double side = double.Parse(Console.ReadLine());
+            area = side * side;
+        }
+        else if (shape == "rectangle")
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            area = length * width;
+        }
+        else if (shape == "circle")
+        {
+            double radius = double.Parse(Console.ReadLine());
+            area = Math.PI * radius * radius;
+        }
+        else if (shape == "triangle")
+        {
+            double baseLength = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            area = 0.5 * baseLength * height;
+        }
+        Console.WriteLine($"{area:F3}");
+    }
+}

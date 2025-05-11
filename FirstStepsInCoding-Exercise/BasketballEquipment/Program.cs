1ef1266b2b3fbec811afa0f371858b782e087c69
Pavsheto <pavsheto@gmail.com>
2025-05-11 10:51:29 +0300

    add Basketball Equipment

---
+++ FirstStepsInCoding-Exercise/BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BasketballEquipment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int annualFee = int.Parse(Console.ReadLine());
+        
+        double sneakersPrice = annualFee * 0.60; // 40% по-малка
+        double outfitPrice = sneakersPrice * 0.80; // 20% по-евтина от кецовете
+        double ballPrice = outfitPrice / 4;
+        double accessoriesPrice = ballPrice / 5;
+        
+        double totalPrice = annualFee + sneakersPrice + outfitPrice + ballPrice + accessoriesPrice;
+        
+        Console.WriteLine($"{totalPrice:F2}");
+    }
+}

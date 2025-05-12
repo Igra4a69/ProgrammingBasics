09363d386c6f7c384c145b698b7ed2c6d4bf54ea
Pavsheto <pavsheto@gmail.com>
2025-05-12 16:53:31 +0300

    add Operations Between Numbers

---
+++ ConditionalStatementsAdvanced-Exercise/OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace OperationsBetweenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N1 = int.Parse(Console.ReadLine());
+        int N2 = int.Parse(Console.ReadLine());
+        string op = Console.ReadLine();
+
+        double result = 0;
+
+        if (op == "+" || op == "-" || op == "*")
+        {
+            if (op == "+")
+            {
+                result = N1 + N2;
+            }
+            else if (op == "-")
+            {
+                result = N1 - N2;
+            }
+            else if (op == "*")
+            {
+                result = N1 * N2;
+            }
+
+            string evenOrOdd = result % 2 == 0 ? "even" : "odd";
+            Console.WriteLine($"{N1} {op} {N2} = {result} - {evenOrOdd}");
+        }
+        else if (op == "/")
+        {
+            if (N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+            else
+            {
+                result = (double)N1 / N2;
+                Console.WriteLine($"{N1} / {N2} = {result:F2}");
+            }
+        }
+        else if (op == "%")
+        {
+            if (N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+            else
+            {
+                result = N1 % N2;
+                Console.WriteLine($"{N1} % {N2} = {result}");
+            }
+        }
+    }
+}

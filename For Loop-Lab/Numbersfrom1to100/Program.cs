0346523481d2b2fa1ce88ba1fab77a31a1ed3016
Pavsheto <pavsheto@gmail.com>
2025-05-14 16:28:49 +0300

    add Numbers from 1 to 100

---
+++ For Loop-Lab/Numbersfrom1to100/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Numbersfrom1to100;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

0e6b4d963e802837a5aa85b6a93df4041a689eb6
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:19:19 +0300

    add Account Balance

---
+++ WhileLoop-Lab/AccountBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AccountBalance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double total = 0;
+        string input;
+
+        while ((input = Console.ReadLine()) != "NoMoreMoney")
+        {
+            double amount = double.Parse(input);
+
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+
+            total += amount;
+            Console.WriteLine($"Increase: {amount:F2}");
+        }
+
+        Console.WriteLine($"Total: {total:F2}");
+    }
+}

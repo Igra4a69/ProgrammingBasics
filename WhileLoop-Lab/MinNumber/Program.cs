83b17ef777eb82603616df48134aaa8d214e7467
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:22:40 +0300

    add Min Number

---
+++ WhileLoop-Lab/MinNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MinNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int minNumber = int.MaxValue;
+        string input;
+
+        while ((input = Console.ReadLine()) != "Stop")
+        {
+            int number = int.Parse(input);
+            if (number < minNumber)
+            {
+                minNumber = number;
+            }
+        }
+
+        Console.WriteLine(minNumber);
+    }
+}

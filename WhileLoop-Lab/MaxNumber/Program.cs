8a3f0a25e5658f9e058e39c03e607a404085172b
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:21:23 +0300

    add Max Number

---
+++ WhileLoop-Lab/MaxNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MaxNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int maxNumber = int.MinValue;
+        string input;
+
+        while ((input = Console.ReadLine()) != "Stop")
+        {
+            int number = int.Parse(input);
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+
+        Console.WriteLine(maxNumber);
+    }
+}

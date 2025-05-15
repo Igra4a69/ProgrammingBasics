a561738e04641d1150237a4614c4fe5a467a2cb7
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:17:30 +0300

    add Sequence 2k+1

---
+++ WhileLoop-Lab/Sequence2k1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sequence2k1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int number = 1;
+
+        while (number <= n)
+        {
+            Console.WriteLine(number);
+            number = number * 2 + 1;
+        }
+    }
+}

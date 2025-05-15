56d8d3f4e5aa240dc9a9789678be5d329db5d80d
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:01:55 +0300

    add Oscars

---
+++ NumbersEndingin7/Oscars/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Oscars;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string actorName = Console.ReadLine();
+        double academyPoints = double.Parse(Console.ReadLine());
+        int numJudges = int.Parse(Console.ReadLine());
+
+        double totalPoints = academyPoints;
+
+        for (int i = 0; i < numJudges; i++)
+        {
+            string judgeName = Console.ReadLine();
+            double judgePoints = double.Parse(Console.ReadLine());
+
+            totalPoints += (judgeName.Length * judgePoints) / 2.0;
+
+            if (totalPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {totalPoints:F1}!");
+                return;
+            }
+        }
+
+        double needed = 1250.5 - totalPoints;
+        Console.WriteLine($"Sorry, {actorName} you need {needed:F1} more!");
+    }
+}

54390e2a517bd8449e6c4434d58f294bc8c77c39
Pavsheto <pavsheto@gmail.com>
2025-05-15 17:16:13 +0300

    add Sum Numbers

---
+++ WhileLoop-Lab/SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SumNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int targetSum = int.Parse(Console.ReadLine());
+        int currentSum = 0;
+
+        while (currentSum < targetSum)
+        {
+            int number = int.Parse(Console.ReadLine());
+            currentSum += number;
+        }
+
+        Console.WriteLine(currentSum);
+    }
+}

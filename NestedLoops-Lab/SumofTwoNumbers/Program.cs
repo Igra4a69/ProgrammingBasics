38a0ea87dc91ecde8736263164f074faf7822c54
Pavsheto <pavsheto@gmail.com>
2025-05-16 15:55:25 +0300

    add Sum of Two Numbers

---
+++ NestedLoops-Lab/SumofTwoNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SumofTwoNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int combinationCount = 0;
+        bool found = false;
+
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                combinationCount++;
+                if (i + j == magicNumber)
+                {
+                    Console.WriteLine($"Combination N:{combinationCount} ({i} + {j} = {magicNumber})");
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine($"{combinationCount} combinations - neither equals {magicNumber}");
+        }
+    }
+}

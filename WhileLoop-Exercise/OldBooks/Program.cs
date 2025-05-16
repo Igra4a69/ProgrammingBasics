7b5844417cfe160a82aa9551dea20d33c9ffaea7
Pavsheto <pavsheto@gmail.com>
2025-05-16 15:20:27 +0300

    add Old Books

---
+++ WhileLoop-Exercise/OldBooks/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OldBooks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string favoriteBook = Console.ReadLine();
+        string input;
+        int checkedBooks = 0;
+
+        while ((input = Console.ReadLine()) != "No More Books")
+        {
+            if (input == favoriteBook)
+            {
+                Console.WriteLine($"You checked {checkedBooks} books and found it.");
+                return;
+            }
+
+            checkedBooks++;
+        }
+
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {checkedBooks} books.");
+    }
+}

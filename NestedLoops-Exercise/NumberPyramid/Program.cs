73647f1c32c8b5912444e5b948e1f6371e24d39a
Pavsheto <pavsheto@gmail.com>
2025-05-16 17:06:48 +0300

    add Number Pyramid

---
+++ NestedLoops-Exercise/NumberPyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NumberPyramid;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int current = 1;
+        bool isFinished = false;
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                if (current > n)
+                {
+                    isFinished = true;
+                    break;
+                }
+
+                Console.Write(current + " ");
+                current++;
+            }
+
+            Console.WriteLine();
+
+            if (isFinished)
+            {
+                break;
+            }
+        }
+    }
+}

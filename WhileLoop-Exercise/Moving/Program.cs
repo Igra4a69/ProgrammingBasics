00e3d1fb1d400a3c60618aceb02bc1d7bd305032
Pavsheto <pavsheto@gmail.com>
2025-05-16 15:37:31 +0300

    add Moving

---
+++ WhileLoop-Exercise/Moving/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Moving;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = int.Parse(Console.ReadLine());
+        int length = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+
+        int freeVolume = width * length * height;
+        string input;
+
+        while ((input = Console.ReadLine()) != "Done")
+        {
+            int boxes = int.Parse(input);
+            freeVolume -= boxes;
+
+            if (freeVolume < 0)
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(freeVolume)} Cubic meters more.");
+                return;
+            }
+        }
+
+        Console.WriteLine($"{freeVolume} Cubic meters left.");
+    }
+}

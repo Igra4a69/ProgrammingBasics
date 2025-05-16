4359a35f951c52906e7319c028aff002d1e9247b
Pavsheto <pavsheto@gmail.com>
2025-05-16 15:57:58 +0300

    Add Travelling

---
+++ NestedLoops-Lab/Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Travelling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string destination = Console.ReadLine();
+            if (destination == "End")
+            {
+                break;
+            }
+
+            double neededBudget = double.Parse(Console.ReadLine());
+            double savedMoney = 0;
+
+            while (savedMoney < neededBudget)
+            {
+                double savedToday = double.Parse(Console.ReadLine());
+                savedMoney += savedToday;
+            }
+
+            Console.WriteLine($"Going to {destination}!");
+        }
+    }
+}

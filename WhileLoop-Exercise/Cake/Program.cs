01b5bd7b384d9372e595ad09cec0d4ead8819577
Pavsheto <pavsheto@gmail.com>
2025-05-16 15:35:45 +0300

    add Cake

---
+++ WhileLoop-Exercise/Cake/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Cake;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = int.Parse(Console.ReadLine());
+        int length = int.Parse(Console.ReadLine());
+
+        int totalPieces = width * length;
+        string input;
+        
+        while ((input = Console.ReadLine()) != "STOP")
+        {
+            int piecesTaken = int.Parse(input);
+            totalPieces -= piecesTaken;
+
+            if (totalPieces < 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(totalPieces)} pieces more.");
+                return;
+            }
+        }
+
+        Console.WriteLine($"{totalPieces} pieces are left.");
+    }
+}

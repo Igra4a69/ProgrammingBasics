b5e8139b7fe2405fdf0ff7b82670eb263371142f
Pavsheto <pavsheto@gmail.com>
2025-05-16 17:15:30 +0300

    add Train The Trainers

---
+++ NestedLoops-Exercise/TrainTheTrainers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TrainTheTrainers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int juryCount = int.Parse(Console.ReadLine());
+
+        double totalSum = 0;
+        int totalGradesCount = 0;
+
+        while (true)
+        {
+            string presentation = Console.ReadLine();
+            if (presentation == "Finish")
+            {
+                break;
+            }
+
+            double sumGrades = 0;
+
+            for (int i = 0; i < juryCount; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                sumGrades += grade;
+                totalSum += grade;
+                totalGradesCount++;
+            }
+
+            double average = sumGrades / juryCount;
+            Console.WriteLine($"{presentation} - {average:F2}.");
+        }
+
+        double finalAverage = totalSum / totalGradesCount;
+        Console.WriteLine($"Student's final assessment is {finalAverage:F2}.");
+    }
+}
